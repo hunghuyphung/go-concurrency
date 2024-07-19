ee6e9053d5d981fd5347a11efa474f6af8ba0e45
hungph3 <hungph3@vnpay.vn>
2024-07-19 09:05:10 +0700

    Initial

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Unbuffered Channel
+	*/
+
+	ch := make(chan int)
+	fmt.Println("Main goroutine is running")
+	ch <- 1
+
+	go func() {
+		fmt.Println("Anonymous goroutine is running")
+		fmt.Println(<-ch)
+	}()
+
+	/*
+		- Result: Deadlock
+		- Explain:
+				1. ch := make(chan int): This creates an unbuffered channel ch that can send and receive integer values.
+				2. fmt.Println("Main goroutine is running"): This prints a message indicating that the main goroutine has started executing.
+				3. ch <- 1: This line attempts to send the value 1 into the channel ch.
+			       However, since this is an unbuffered channel and there's no receiver ready yet.
+			       So, this operation will block the main goroutine => the next commands are no execute
+
+		- Fix:
+		        1. Create another goroutine that precedes the operation ch <- 1
+	*/
+
+	myCh := make(chan int)
+	fmt.Println("Main goroutine is running")
+
+	go func() {
+		fmt.Println("Anonymous goroutine is running")
+		fmt.Println(<-myCh)
+	}()
+
+	myCh <- 1
+}
